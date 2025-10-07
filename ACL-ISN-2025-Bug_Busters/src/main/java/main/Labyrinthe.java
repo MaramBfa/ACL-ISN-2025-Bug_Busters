c28627808e6c3f2c73d120b2e39fd842b72c82bf
Ibtissam567 <eddadsiibtissam0@gmail.com>
2025-10-07 15:03:41 +0200

    Update Labyrinthe.java

--- ACL-ISN-2025-Bug_Busters/src/main/java/main/Labyrinthe.java
+++ ACL-ISN-2025-Bug_Busters/src/main/java/main/Labyrinthe.java
@@ -1,45 +1,127 @@
-package main;
-
-import simple.Position;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Labyrinthe {
 
     private char[][] grille;
-    private int width, height;
-    private Random rand = new Random();
+    private int lignes;
+    private int colonnes;
 
-    public Labyrinthe(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.grille = new char[height][width];
-        genererGrille();
+    public static final char MUR = '#';
+    public static final char VIDE = ' ';
+    public static final char HERO = 'H';
+    public static final char MONSTRE = 'M';
+    public static final char TRESOR = 'T';
+
+    
+    public Labyrinthe(int lignes, int colonnes) {
+        this.lignes = lignes;
+        this.colonnes = colonnes;
+        this.grille = new char[lignes][colonnes];
+        genererLabyrintheDefaut();
     }
 
-    /** Génération aléatoire de la grille */
-    private void genererGrille() {
-        for (int x = 0; x < height; x++) {
-            for (int y = 0; y < width; y++) {
-                // Bordures = murs
-                if (x == 0 || y == 0 || x == height - 1 || y == width - 1) {
-                    grille[x][y] = '#';
-                } else {
-                    grille[x][y] = (rand.nextDouble() < 0.25) ? '#' : ' '; // 25% de murs
-                }
+    /**
+     * Constructeur pour charger un labyrinthe à partir d’un fichier texte
+     */
+    public Labyrinthe(String nomFichier) throws IOException {
+        chargerDepuisFichier(nomFichier);
+    }
+
+    /**
+     * Génère un labyrinthe simple par défaut
+     */
+    private void genererLabyrintheDefaut() {
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
+                if (i == 0 || j == 0 || i == lignes - 1 || j == colonnes - 1)
+                    grille[i][j] = MUR;
+                else
+                    grille[i][j] = VIDE;
             }
+        }
+        // Placer le héros et le trésor
+        grille[1][1] = HERO;
+        grille[lignes - 2][colonnes - 2] = TRESOR;
+    }
+
+    /**
+     * Charge un labyrinthe depuis un fichier texte
+     */
+    private void chargerDepuisFichier(String nomFichier) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(nomFichier));
+        String ligne;
+        int i = 0;
+
+        // Lire toutes les lignes du fichier
+        grille = new char[50][50]; // taille max temporaire
+        while ((ligne = br.readLine()) != null) {
+            colonnes = ligne.length();
+            for (int j = 0; j < colonnes; j++) {
+                grille[i][j] = ligne.charAt(j);
+            }
+            i++;
+        }
+        lignes = i;
+        br.close();
+    }
+
+    /**
+     * Affiche le labyrinthe dans la console
+     */
+    public void afficher() {
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
+                System.out.print(grille[i][j]);
+            }
+            System.out.println();
         }
     }
 
-    public char[][] getGrille() { return grille; }
-
-    /** Placer un élément aléatoirement sur une case vide */
-    public Position placerAleatoirement() {
-        Position pos;
-        do {
-            int x = rand.nextInt(height);
-            int y = rand.nextInt(width);
-            pos = new Position(x, y);
-        } while (grille[pos.x][pos.y] != ' ');
-        return pos;
+    /**
+     * Vérifie si une case est libre (non mur)
+     */
+    public boolean estLibre(int x, int y) {
+        if (x < 0 || y < 0 || x >= lignes || y >= colonnes) return false;
+        return grille[x][y] != MUR;
     }
+
+    /**
+     * Déplace le héros dans une direction (haut, bas, gauche, droite)
+     */
+    public void deplacerHero(String direction) {
+        int heroX = -1, heroY = -1;
+
+        // Trouver le héros
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
+                if (grille[i][j] == HERO) {
+                    heroX = i;
+                    heroY = j;
+                    break;
+                }
+            }
+        }
+
+        if (heroX == -1) return; // pas de héros trouvé
+
+        int nx = heroX, ny = heroY;
+        switch (direction.toLowerCase()) {
+            case "haut": nx--; break;
+            case "bas": nx++; break;
+            case "gauche": ny--; break;
+            case "droite": ny++; break;
+        }
+
+        // Vérifie si la nouvelle position est libre
+        if (estLibre(nx, ny)) {
+            grille[heroX][heroY] = VIDE;
+            grille[nx][ny] = HERO;
+        }
+    }
+
+    // Getters
+    public int getLignes() { return lignes; }
+    public int getColonnes() { return colonnes; }
 }
