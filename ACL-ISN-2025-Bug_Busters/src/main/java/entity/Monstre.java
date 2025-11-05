54018d69e4d825685e44088251fe7ad2a23d3ef8
ayabenhilal <ayabenhilal12@gmail.com>
2025-11-05 21:19:30 +0100

    Update Monstre.java

--- ACL-ISN-2025-Bug_Busters/src/main/java/entity/Monstre.java
+++ ACL-ISN-2025-Bug_Busters/src/main/java/entity/Monstre.java
@@ -5,22 +5,31 @@ import java.util.Random;
 
 public class Monstre {
     private Point position;
-    private boolean estVivant = true;
-    private Random rand = new Random();
+    private boolean estVivant;
+    private Random rand;
 
     public Monstre(int gridWidth, int gridHeight, int heroX, int heroY) {
+        rand = new Random();
+        estVivant = true;
         int x, y;
         do {
-            x = rand.nextInt(gridWidth);
-            y = rand.nextInt(gridHeight);
+            x = rand.nextInt(gridHeight);
+            y = rand.nextInt(gridWidth);
         } while (x == heroX && y == heroY);
         position = new Point(x, y);
     }
 
+    // === Position ===
     public Point getPosition() {
         return position;
     }
 
+    public void setPosition(int x, int y) {
+        position.x = x;
+        position.y = y;
+    }
+
+    // === État de vie ===
     public boolean estVivant() {
         return estVivant;
     }
@@ -31,14 +40,16 @@ public class Monstre {
         position.y = -1;
     }
 
+    // === Déplacement aléatoire ===
     public void move(int gridWidth, int gridHeight) {
         if (!estVivant) return;
+
         int dir = rand.nextInt(4);
         switch (dir) {
-            case 0: if (position.y > 0) position.y--; break;
-            case 1: if (position.y < gridHeight - 1) position.y++; break;
-            case 2: if (position.x > 0) position.x--; break;
-            case 3: if (position.x < gridWidth - 1) position.x++; break;
+            case 0 -> { if (position.y > 0) position.y--; } // gauche
+            case 1 -> { if (position.y < gridWidth - 1) position.y++; } // droite
+            case 2 -> { if (position.x > 0) position.x--; } // haut
+            case 3 -> { if (position.x < gridHeight - 1) position.x++; } // bas
         }
     }
 }
